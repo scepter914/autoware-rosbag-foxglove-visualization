bb47cb19c46d5d206ce0a5e0571742e77d49307a
scepter914 <scepter914@gmail.com>
2022-07-22 17:37:10 +0900

    update record scripts
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- scripts/rosbag_record.sh
+++ scripts/rosbag_record.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
+#ROSBAG_NAME=$(basename $1) 
+ROSBAG_NAME=$(basename $1) 
+ROSBAG_NAME_WITHOUT_EXT=${ROSBAG_NAME%.*}".txt"
+
+OUTPUT_ROOT_DIR="$HOME/Downloads/"
 VISUALIZE_SUBDIR="visualization/"
-ROSBAG_NAME=$1
-VISUALIZE_ROSBAG_DIR=$1$VISUALIZE_SUBDIR
-ROSBAG_PATH=$VISUALIZE_ROSBAG_DIR$ROSBAG_NAME
-mkdir -p $VISUALIZE_ROSBAG_DIR
+OUTPUT_NAME=$(date +%Y%m%d-%H%M%S)
+
+
+ROSBAG_PATH=$OUTPUT_ROOT_DIR$VISUALIZE_SUBDIR$OUTPUT_NAME
+TOUCH_PATH=$OUTPUT_ROOT_DIR$VISUALIZE_SUBDIR$OUTPUT_NAME"/"$ROSBAG_NAME_WITHOUT_EXT
+
+#mkdir -p $VISUALIZE_ROSBAG_DIR
+
+
+
 
 RECORDTOPIC=(
 /tf
 /tf_static
-/perception/*
+/localization/kinematic_state
+/sensing/lidar/concatenated/pointcloud
 )
 
 ALLTOPIC=""
+PERCEPTION_TOPIC=$(ros2 topic list | grep --regexp="/perception/object_recognition/*")
+ALLTOPIC="$ALLTOPIC $PERCEPTION_TOPIC"
 
 for i in ${RECORDTOPIC[@]}; do
   ALLTOPIC="$ALLTOPIC $i"
 done
 
+echo $ALLTOPIC
 ros2 bag record $ALLTOPIC -o $ROSBAG_PATH
+
+# touch original rosbag path
+touch $TOUCH_PATH
+
